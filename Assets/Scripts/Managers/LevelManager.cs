e514d64810aeadf6a4276e43ca39802c942422b7
unknown <massimo.buono97@gmail.com>
2021-11-17 01:27:49 +0100

    added level 5-6-7

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -7,7 +7,7 @@ using UnityEngine.SceneManagement;
 public class LevelManager : MonoBehaviour
 {
     private static int _nextLevel = 0;
-    private static int _maxLevel = 4;
+    private static int _maxLevel = 7;
     private static int _levelPlayed = -1;
 
     private static string SceneName(int level)
