3e044c06f07fce9cda1cd1e27fa311ccdd8f7e0b
xbasciox <50500266+xbasciox@users.noreply.github.com>
2021-11-29 19:17:02 +0100

    jump magnet after FPS fix

--- Assets/Scripts/General/GameManager.cs
+++ Assets/Scripts/General/GameManager.cs
@@ -52,8 +52,8 @@ public class GameManager : MonoBehaviour
         }
 
         // test different frame rates
-        //QualitySettings.vSyncCount = 0;  // VSync must be disabled
-        //Application.targetFrameRate = 100;
+        QualitySettings.vSyncCount = 0;  // VSync must be disabled
+        Application.targetFrameRate = 1000;
 
 
         _dataManager = FindObjectOfType<DataManager>();
